69c89c9b065164126b5f40ad802b72c3ad3658c0
Bruno Romanski <romanskibruno@gmail.com>
2023-11-16 14:07:38 +0000

    Navigating back using HeaderBack

--- src/components/header/HeaderBack.js
+++ src/components/header/HeaderBack.js
@@ -2,9 +2,12 @@ import { View, Text } from "react-native";
 import { IconButton } from "react-native-paper";
 import { palette } from "../../style";
 import { StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 const HeaderBack = (props) => {
-    const {style, children, ...rest} = props
+    const navigation = useNavigation();
+
+    const {style, children, ...rest} = props;
     return (
         <View style={{...styles.header, ...style}} {...rest}>
             <IconButton
@@ -12,7 +15,7 @@ const HeaderBack = (props) => {
                 icon={require('../../../assets/icons/chevron_left.png')}
                 iconColor={palette.black}
                 size={4 * vh}
-                onPress={() => console.log('Pressed')}
+                onPress={() => navigation.goBack()}
             />
             <Text style={styles.title}>{children}</Text>
         </View>
