9ae2b968e23fde8a283af7d03499e4bb48d67517
Bruno Romanski <romanskibruno@gmail.com>
2023-11-16 18:37:30 +0000

    Styled the login screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,12 +1,39 @@
-import { View } from "react-native";
+import { View, Keyboard, TouchableWithoutFeedback } from "react-native";
+import { Button, HeaderBack, PasswordInput, TextInput } from "../components";
 import { StyleSheet } from "react-native";
-import { HeaderBack } from "../components";
+import { palette, themes } from "../style";
 
 export default LoginScreen = ({ navigation }) => {
     return (
-        <View style={styles.page}>
-            <HeaderBack>Login</HeaderBack>
-        </View>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+            <View style={styles.page}>
+                <HeaderBack>Sign in</HeaderBack>
+                <View style={styles.inputGroup}>
+                    <TextInput style={styles.textInput} theme={themes.textInput} mode='outlined' label="Work email" placeholder='name@workmail.com'/>
+                    <View>
+                        <PasswordInput style={styles.textInput} theme={themes.textInput} mode='outlined' label="Password" />
+                        <Button
+                            onPress={() => {}}
+                            mode='text'
+                            theme={themes.button}
+                            style={styles.forgotButton}
+                        >
+                            Forgot your password?
+                        </Button>
+                    </View>
+                    
+                </View>
+                <Button
+                    onPress={() => {}}
+                    mode='contained'
+                    theme={themes.button}
+                    style={styles.button}
+                    labelStyle={{ marginHorizontal: 0 }}
+                >
+                    Sign in
+                </Button>
+            </View>
+        </TouchableWithoutFeedback>
     );
 }
 
@@ -15,6 +42,27 @@ const styles = StyleSheet.create({
         marginTop: 5 * vh,
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
+        backgroundColor: palette.white
+    },
+    inputGroup: {
+        gap: 3.5 * vh
+    },
+    textInput: {
+        width: 80 * vmin,
+        height: 12 * vmin,
+        backgroundColor: palette.white
+    },
+    forgotButton: {
+        alignSelf: 'flex-end',
+        margin: 0, padding: 0, borderWidth: 0, borderRadius: 0,
+    },
+    button: {
+        width: 70 * vmin,
+        height: 14 * vmin,
+        justifyContent: 'center',
+        paddingBottom: 0.5 * vmin,
+        marginTop: 5 * vh,
+        marginBottom: 5 * vh
     }
 })
--- src/style/palette.js
+++ src/style/palette.js
@@ -2,5 +2,6 @@ export const palette = {
     white: '#f5f5f5',
     black: '#111111',
     lightGrey: '#D3D3D3',
+    grey: '#8A8A8A',
     purple: '#503CC8'
 }
