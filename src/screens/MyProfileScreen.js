3dbd520927e80cdba0bea7dcfc2dcc730785a536
Puspita <puspitagoswami14@gmail.com>
2023-11-22 22:43:23 +0000

    Profile text done

--- src/screens/MyProfileScreen.js
+++ src/screens/MyProfileScreen.js
@@ -6,11 +6,15 @@ import { dimensions } from "../style";
 export default MyProfileScreen = ({ navigation }) => {
   return (
     <View style={styles.page}>
-      <View>
+      <View style={styles.profilePicContainer}>
         <Image
           style={styles.profilePic}
           source={require("../../assets/images/welcome_screen/profile-pic.png")}
         />
+        <View style={styles.profileTextContainer}>
+          <Text style={styles.profileName}>Jessica Wang</Text>
+          <Text style={styles.profileEmail}>jessica@facebook.com</Text>
+        </View>
       </View>
     </View>
   );
@@ -18,15 +22,36 @@ export default MyProfileScreen = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   page: {
-    marginTop: 15 * vh,
+    marginTop: 20 * vh,
     flex: 1,
     alignItems: "center",
     justifyContent: "space-between",
     backgroundColor: palette.white,
   },
+  profilePicContainer:{
+    justifyContent:'center',
+    alignItems:'center'
+  },
   profilePic: {
     width: 140,
     height: 140,
-    borderRadius:70,
+    borderRadius: 70,
   },
+  profileTextContainer:{
+    marginTop: 2 * vh,
+    justifyContent:"center",
+    alignItems:"center",
+  },
+  profileName:{
+    fontWeight:'700',
+    fontFamily:'Lato-Regular',
+    fontSize:  5 * vmin,
+
+  },
+  profileEmail:{
+    color:'#8A8A8A',
+    fontSize: 3.8 * vmin,
+    letterSpacing:1,
+    fontWeight:'600'
+  }
 });
