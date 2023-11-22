d8c456653b013b26d4fba4833244d797f70afbec
Puspita <puspitagoswami14@gmail.com>
2023-11-22 22:07:54 +0000

    Profile picture added

--- src/screens/MyProfileScreen.js
+++ src/screens/MyProfileScreen.js
@@ -1,13 +1,32 @@
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Image } from "react-native";
+import { palette } from "../style";
+import { themes } from "../style";
+import { dimensions } from "../style";
 
-export default MyProfileScreen=({navigation})=>{
-    return(
-        <View>
-            <Text>Profile</Text>
-        </View>
-    );
-}
+export default MyProfileScreen = ({ navigation }) => {
+  return (
+    <View style={styles.page}>
+      <View>
+        <Image
+          style={styles.profilePic}
+          source={require("../../assets/images/welcome_screen/profile-pic.png")}
+        />
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-
-})
+  page: {
+    marginTop: 15 * vh,
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-between",
+    backgroundColor: palette.white,
+  },
+  profilePic: {
+    width: 140,
+    height: 140,
+    borderRadius:70,
+  },
+});
