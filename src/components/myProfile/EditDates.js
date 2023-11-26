71fe40fb39b7c70d20de73ff491c6b15418d2503
Puspita <puspitagoswami14@gmail.com>
2023-11-26 00:00:05 +0000

    color set

--- src/components/myProfile/EditDates.js
+++ src/components/myProfile/EditDates.js
@@ -1,22 +1,20 @@
-import { View, Text, StyleSheet,Image } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 export default EditDates = () => {
   return (
     <View>
       <Text style={styles.headingText}>Edit days off calendar</Text>
       <View style={styles.dateContainer}>
-        
-            <Text style={styles.textContainer}>11 - 18 Nov</Text>
-      
-  
-            <Text style={styles.textContainer}>02 - 10 Jan</Text>
-      
+        <Text style={styles.textContainer}>11 - 18 Nov</Text>
+
+        <Text style={styles.textContainer}>02 - 10 Jan</Text>
+
         <View>
-            <Image
-              style={styles.icon}
-              source={require("../../../assets/images/welcome_screen/calender.png")}
-            />
-          </View>
+          <Image
+            style={styles.icon}
+            source={require("../../../assets/images/welcome_screen/calender.png")}
+          />
+        </View>
       </View>
     </View>
   );
@@ -32,14 +30,14 @@ const styles = StyleSheet.create({
     fontWeight: "700",
     color: "#000000",
   },
-  dateContainer:{
+  dateContainer: {
     marginTop: 2.5 * vh,
-    flexDirection:'row',
-    justifyContent:'space-between'
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   textContainer: {
-    color: "#A9A9A9",
-    backgroundColor: "#F2F0F0",
+    color: "#503CC8",
+    backgroundColor: "#EEECFA",
     padding: 5,
     borderRadius: 5,
     paddingLeft: 15,
