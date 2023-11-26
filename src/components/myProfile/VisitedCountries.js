bc76fe89cf22a7c65e28a5906f191d535c9a0573
Puspita <puspitagoswami14@gmail.com>
2023-11-26 21:25:33 +0000

    Visited countries UI completed

--- src/components/myProfile/VisitedCountries.js
+++ src/components/myProfile/VisitedCountries.js
@@ -1,15 +1,44 @@
-import {View,Image, StyleSheet} from 'react-native';
+import { View, Image, StyleSheet, Text } from "react-native";
 
-export default VisitedCountries = () =>{
-    return(
-  <View>
-    <Image source={require("../../../assets/images/profileScreen/AustraliaFlag.png")}/>
-    <Image source={require("../../../assets/images/profileScreen/FranceFlag.png")}/>
-    <Image source={require("../../../assets/images/profileScreen/JapanFlag.png")}/>
-    <Image source={require("../../../assets/images/profileScreen/BrazilFlag.png")}/>
-  </View>
-)
-    }
+export default VisitedCountries = () => {
+  return (
+    <View style={styles.visitedWrap}>
+        <View>
+            <Text style={styles.headingText}>Countries visited on DayOff</Text>
+        </View>
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("../../../assets/images/profileScreen/AustraliaFlag.png")}
+        />
+        <Image
+          source={require("../../../assets/images/profileScreen/FranceFlag.png")}
+        />
+        <Image
+          source={require("../../../assets/images/profileScreen/JapanFlag.png")}
+        />
+        <Image
+          source={require("../../../assets/images/profileScreen/BrazilFlag.png")}
+        />
+      </View>
+    </View>
+  );
+};
 const styles = StyleSheet.create({
-
-})
+    visitedWrap:{
+        
+    },
+    headingText: {
+        marginTop: 3 * vh,
+        marginBottom: 3 * vh,
+        alignSelf: "center",
+        width: 85 * vmin,
+        fontFamily: "Lato-Regular",
+        fontSize: 4.5 * vmin,
+        fontWeight: "700",
+        color: "#000000",
+      },
+  imageContainer: {
+    flexDirection: "row",
+    gap:15,
+  },
+});
