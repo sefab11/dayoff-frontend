aba59510b9f9d906250e2e79f6182f7e1e202f11
Bruno Romanski <romanskibruno@gmail.com>
2023-12-14 21:44:55 +0000

    Added a CountryMultiSelector component for selecting countries to visit

---
+++ src/components/countrySelector/countryMultiSelector.js
@@ -0,0 +1,14 @@
+import SectionedMultiSelect from "react-native-sectioned-multi-select";
+import CountryCodes from '../../../assets/CountryCodes.json'
+
+const CountryMultiSelector = (props) => {
+    const {style, children, ...rest} = props
+    
+    return (
+        <SectionedMultiSelect
+            items={}
+        />
+    )
+}
+
+export default CountryMultiSelector;
---
+++ src/components/countrySelector/index.js
@@ -0,0 +1,1 @@
+export {default as CountryMultiSelector} from './countryMultiSelector'
