ac2245b61e88a6005a5ad5ea87a96a564d0cae39
Sepehr <51531251+Sepehr-Boz@users.noreply.github.com>
2024-03-19 18:58:09 +0000

    ChatFooter Update1
    added image picker and emoji picker to chat footer.

--- src/components/chat/ChatFooter.js
+++ src/components/chat/ChatFooter.js
@@ -1,15 +1,21 @@
 import { IconButton } from 'react-native-paper';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, ScrollView, TouchableWithoutFeedback } from 'react-native';
 import { dimensions, palette } from '../../style';
 import { MessageInput } from '../textinput';
 import React, { useState, useEffect } from "react";
+import * as ImagePicker from 'expo-image-picker';
+import EmojiPicker from 'rn-emoji-keyboard'
 
 [vw, vh, vmin, vmax] = dimensions
 
 const ChatFooter = (props) => {
     const {style, ...rest} = props;
 
+    const [image, setImage] = useState(null);
     const [message, setMessage] = useState("");
+
+    const [showEmojis, setShowEmojis] = useState(false);
+    const toggleShowEmojis = () => setShowEmojis(!showEmojis);
 
     const sendMessage = () => {
         // send message to chat screen
@@ -18,44 +24,76 @@ const ChatFooter = (props) => {
         setMessage("");
     }
 
+    useEffect(() => {
+        //TODO: whenever image is updated then prepend it to the message
+        console.log(image)
+    }, [image])
+
+
+
     return (
-        <View style={{...styles.footer, ...style}} {...rest}>
-            <IconButton
-                style={{...styles.icon, padding: 10}}
-                icon={require('../../../assets/icons/plus.png')}
-                iconColor={palette.purple}
-                size={3 * vh}
-                /* TODO: add image to message on press */
-                onPress={() => {}}
-            />
-            {/* TODO: return message on enter */}
-            <MessageInput
-                value={message}
-                setValue={setMessage}
-                sentMessage={sendMessage}
-            />
-            <IconButton
-                style={styles.icon}
-                icon={require('../../../assets/icons/smile.png')}
-                iconColor={palette.purple}
-                size={3 * vh}
-                /* TODO: add emoji to message on press */
-                onPress={() => {}}
-            />
-            <IconButton
-                style={styles.icon}
-                icon={require('../../../assets/icons/mic.png')}
-                iconColor={palette.purple}
-                size={3 * vh}
-                /* TODO: add voice recording to message */
-                onPress={() => {}}
-            />
+        <View style={{...styles.footer, ...style}} {...rest} paddingBottom={showEmojis ? 40 * vh: 4 * vmin}>
+            {// dont show regular footer when emojis selected
+            showEmojis ?
+            <EmojiPicker onEmojiSelected={(e) => {
+                // TODO: update message on emoji selected
+                console.log(e);
+                toggleShowEmojis();
+            }}
+            open={showEmojis}
+            onClose={() => toggleShowEmojis()} />
+            :
+            <>
+                <IconButton
+                    style={{...styles.icon, padding: 10}}
+                    icon={require('../../../assets/icons/plus.png')}
+                    iconColor={palette.purple}
+                    size={3 * vh}
+                    /* TODO: add image to message on press */
+                    onPress={async () => {
+                        let result = await ImagePicker.launchImageLibraryAsync({
+                            mediaTypes: ImagePicker.MediaTypeOptions.All,
+                            allowsEditing: false,
+                            aspect: [4, 3],
+                            quality: 1,
+                        });
+
+                        if (!result.canceled){
+                            setImage(result.assets[0].uri);
+                        }
+                    }}
+                />
+                <MessageInput
+                    /* returns sentMessage on enter */
+                    value={message}
+                    setValue={setMessage}
+                    sentMessage={sendMessage}
+                />
+                <IconButton
+                    style={styles.icon}
+                    icon={require('../../../assets/icons/smile.png')}
+                    iconColor={palette.purple}
+                    size={3 * vh}
+                    /* toggle emoji selector */
+                    onPress={() => toggleShowEmojis()}
+                />
+                <IconButton
+                    style={styles.icon}
+                    icon={require('../../../assets/icons/mic.png')}
+                    iconColor={palette.purple}
+                    size={3 * vh}
+                    /* TODO: add voice recording to message */
+                    onPress={() => {}}
+                />
+            </>
+            }
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     footer: {
+        display: 'flex',
         flexDirection: 'row',
         width: 100 * vw,
         alignItems: 'center',
